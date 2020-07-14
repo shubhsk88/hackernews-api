1c3574325e1e9cfffcea92e259f2a4013e307a55
Shubham Singh <shubham88singh@gmail.com>
2020-07-14 12:42:46 +0530

    defining the mutation of the api

--- src/index.js
+++ src/index.js
@@ -1,17 +1,5 @@
 const { GraphQLServer } = require('graphql-yoga');
 
-const typeDefs = `
-type Query {
-    info: String!
-    feed:[Link!]!
-}
-type Link{
-  id:ID!
-  description:String!
-  url:String!
-}
-`;
-
 const links = [
   {
     id: 'link-0',
@@ -19,18 +7,46 @@ const links = [
     description: 'The component based library for developers',
   },
 ];
+let idCount = links.length;
 const resolvers = {
   Query: {
     feed: () => links,
     info: () => `This is API of hackernews clone`,
   },
-  Link: {
-    id: (parent) => parent.id,
-    description: (parent) => parent.description,
-    url: (parent) => parent.url,
+  Mutation: {
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      };
+      links.push(link);
+      return link;
+    },
+    deleteLink: (parent, args) => {
+      let count;
+      for (let i = 0; i < links.length; i++) {
+        if (links[i].id === args.id) {
+          count = i;
+        }
+      }
+      return links.splice(count, 1);
+    },
+    updateLink: (parent, args) => {
+      for (let i = 0; i < links.length; i++) {
+        if (links[i].id === args.id) {
+          links[i].description = args.description;
+          links[i].url = args.url;
+          return links[i];
+        }
+      }
+    },
   },
 };
 
-const server = new GraphQLServer({ typeDefs, resolvers });
+const server = new GraphQLServer({
+  typeDefs: './src/schema.graphql',
+  resolvers,
+});
 
 server.start(() => console.log('Server is listening to the port: 4000'));
